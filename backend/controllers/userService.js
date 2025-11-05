e4b78b827603012c8475c9b5e5a72f2d6a31be17
Arif Patel <patelarif9498@gmail.com>
2025-11-05 21:50:41 +0530

    audit logs fixes and added logs for users

--- backend/controllers/userService.js
+++ backend/controllers/userService.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+const createAuditLog  = require("../utils/auditLog"); 
 exports.createUser = async (req, res) => {
   try {
     const { name, email, password, userRole } = req.body;
@@ -13,6 +14,17 @@ exports.createUser = async (req, res) => {
     }
     const hashed = password ? await bcrypt.hash(password, 10) : "";
     const user = await User.create({ name, email, password: hashed, userRole });
+    const loggedInUser = await User.findById(req.user?.id).select("name email");
+
+    await createAuditLog(
+      req.user?.id || null,
+      req.user?.userRole || null,
+      `User "${user.name || user.email}" created by ${loggedInUser?.name || loggedInUser?.email || ""}`,
+      "User",
+      user._id,
+      { after: user }
+    );
+
     res.status(201).json(user);
   } catch (err) {
     console.error(err);
@@ -48,10 +60,27 @@ exports.updateUser = async (req, res) => {
     if (password) {
       updateData.password = await bcrypt.hash(password, 10);
     }
+    const existingUser = await User.findById(req.params.id);
 
+    if (!existingUser) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
     const updated = await User.findByIdAndUpdate(req.params.id, updateData, {
       new: true,
     });
+    const loggedInUser = await User.findById(req.user?.id).select("name email");
+    await createAuditLog(
+      req.user?.id || null,
+      req.user?.userRole || null,
+      `User "${updated.name || updated.email}" updated by ${
+        loggedInUser?.name || loggedInUser?.email || ""
+      }`,
+      "User",
+      updated._id,
+      { before: existingUser, after: updated }
+    );
 
     res.json(updated);
   } catch (err) {
@@ -61,7 +90,26 @@ exports.updateUser = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
   try {
-    await User.findByIdAndDelete(req.params.id);
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const loggedInUser = await User.findById(req.user?.id).select("name email");
+
+    await createAuditLog(
+      req.user?.id || null,
+      req.user?.userRole || null,
+      `User "${user.name || user.email}" deleted by ${
+        loggedInUser?.name || loggedInUser?.email || ""
+      }`,
+      "User",
+      user._id,
+      { before: user }
+    );
     res.json({ message: "User deleted successfully" });
   } catch (err) {
     res.status(500).json({ error: err.message });
