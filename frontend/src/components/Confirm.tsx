1e60b78dd80227a0c9f261706853464cf9d8efaa
Arif Patel <patelarif9498@gmail.com>
2025-11-11 13:35:14 +0530

    btn css fixes all over models

--- frontend/src/components/Confirm.tsx
+++ frontend/src/components/Confirm.tsx
@@ -19,9 +19,7 @@ const Confirm = ({
   confirmAlert({
     customUI: ({ onClose }) => (
       <div className="bg-white rounded-xl shadow-xl p-6 w-[90%]  mx-auto text-center  ">
-        <h2 className="text-lg font-semibold mb-2 text-red-600">
-          {title}
-        </h2>
+        <h2 className="text-lg font-semibold mb-2 text-red-600">{title}</h2>
         <p className="mb-5 text-sm">{message}</p>
         <div className="flex justify-center gap-3">
           <button
@@ -29,12 +27,13 @@ const Confirm = ({
               await onConfirm();
               onClose();
             }}
-            className="px-4 py-2 font-bold bg-green-700 text-white rounded-lg hover:bg-green-800 transition flex items-center gap-2">
+            className="px-4 py-2 font-bold bg-green-700 text-white rounded-lg hover:bg-green-800 transition flex items-center gap-2"
+          >
             {confirmText}
           </button>
           <button
             onClick={onClose}
-            className="px-4 py-2 font-bold bg-red-500 text-white rounded-lg hover:bg-red-700 transition"
+            className="px-4 py-2 font-bold bg-[#dc2626] text-white rounded-lg hover:bg-red-700 transition"
           >
             {cancelText}
           </button>
--- frontend/src/components/PaymentModel.tsx
+++ frontend/src/components/PaymentModel.tsx
@@ -189,7 +189,7 @@ useEffect(() => {
             <button
               onClick={onClose}
               disabled={loading}
-              className="px-4 py-2 font-bold bg-red-500 text-white rounded-lg hover:bg-red-700 transition"
+              className="px-4 py-2 font-bold bg-[#dc2626] text-white rounded-lg hover:bg-red-700 transition"
             >
               Cancel
             </button>
