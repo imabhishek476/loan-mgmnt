dc3fba6ef4e5c9f7def7709abfa51f4fa7b41c9c
Arif Patel <patelarif9498@gmail.com>
2025-11-11 15:25:33 +0530

    logs delete after 30 days work completed

--- backend/models/AuditLog.js
+++ backend/models/AuditLog.js
@@ -7,9 +7,13 @@ const auditLogSchema = new mongoose.Schema(
     entityId: { type: mongoose.Schema.Types.ObjectId },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     data: { type: Object },
-    message: { type: String }, 
+    message: { type: String },
+    expireAt: {
+      type: Date,
+      index: { expires: 0 },
+    },
   },
   { timestamps: true }
 );
-
+auditLogSchema.index({ expireAt: 1 }, { expireAfterSeconds: 0 });
 module.exports = mongoose.model("AuditLog", auditLogSchema);
